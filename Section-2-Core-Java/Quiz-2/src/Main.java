42a278af1a6007c7337184d007d42e6990947ea5
Mohamed Elnaggar <>
2026-07-23 02:00:43 +0300

    quiz 2

---
+++ Section-2-Core-Java/Quiz-2/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String [] args) {
+        int a = 20, b = 10;
+
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+
+        System.out.println("Is a greater than b? " + (a > b));
+        System.out.println("Is a > b and b > 0? " + (a > b && b > 0));
+    }
+}
