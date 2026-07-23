007e7cbcefef032176f0a7600a814ccd1b117db2
Mohamed Elnaggar <>
2026-07-23 04:08:14 +0300

    add exercise 5

---
+++ Section-2-Core-Java/Exercise-05/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    public static void main(String [] args) {
+        double num1 = 7;
+        double num2 = 3;
+        char operator = '+';
+        String again = "y";
+
+        while(again == "y") {
+            if(operator == '+')
+                System.out.println("Result: " + (num1 + num2));
+            else if(operator == '-')
+                System.out.println("Result: " + (num1 - num2));
+            else if(operator == '*')
+                System.out.println("Result: " + (num1 * num2));
+            else if(operator == '/') {
+                if(num2 == 0)
+                    System.out.println("Cannot divide by zero.");
+                else
+                    System.out.println("Result: " + (num1 / num2));
+            }
+            again = "n";
+        }
+
+        System.out.println("Thank you for using the calculator.");
+    }
+}
